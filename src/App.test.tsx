ee32c1d8d6129483ff34f3dc89e510bc6212e835
Franklin Martins <franklinsvmartins@hotmail.com>
2022-07-21 08:26:00 -0300

    Final game

---
+++ src/App.test.tsx
@@ -0,0 +1,58 @@
+import { describe, expect, test } from "vitest";
+import App from "./App";
+import { useStore } from "./store";
+import { render, screen, userEvent, within } from "./test/test-utils";
+
+describe("App", () => {
+  test("the title is visible", () => {
+    render(<App />);
+    expect(screen.getByText(/MyWordle/i)).toBeInTheDocument();
+  });
+
+  test("shows empty state", () => {
+    useStore.getState().newGame([]);
+    render(<App />);
+    expect(screen.queryByText("Game Over")).toBeNull();
+    expect(document.querySelectorAll("main div")).toHaveLength(6);
+    expect(document.querySelector("main")?.textContent).toEqual("");
+  });
+
+  test("shows one row of guesses", () => {
+    useStore.getState().newGame(["hello"]);
+    render(<App />);
+    expect(document.querySelector("main")?.textContent).toEqual("hello");
+  });
+
+  test("shows lost game state", () => {
+    useStore.getState().newGame(Array(6).fill("hello"));
+    render(<App />);
+    expect(screen.getByText("Game Over")).toBeInTheDocument();
+  });
+
+  test("show won game state", () => {
+    const initialState = Array(2).fill("hello");
+    useStore.getState().newGame(initialState);
+    const answer = useStore.getState().answer;
+    useStore.getState().addGuess(answer);
+
+    render(<App />);
+
+    // shows all guesses in the DOM
+    expect(document.querySelector("main")?.textContent).toEqual(
+      initialState.join("") + answer
+    );
+    expect(screen.getByText("Game Over")).toBeInTheDocument();
+  });
+
+  test("can start new game", () => {
+    useStore.getState().newGame(Array(6).fill("hello"));
+    render(<App />);
+    expect(screen.getByText("Game Over")).toBeInTheDocument();
+
+    userEvent.click(
+      within(screen.getByRole("modal")).getByText("New Game", {})
+    );
+    expect(document.querySelector("main")?.textContent).toEqual("");
+    expect(screen.queryByText("Game Over")).toBeNull();
+  });
+});
--- src/WordRow.tsx
+++ src/WordRow.tsx
@@ -1,31 +1,49 @@
-const LETTER_LENGTH = 5;
+import { answerSelector, useStore, WORD_LENGTH } from "./store";
+import { LetterState } from "./word-utils";
 
 interface WordRowProps {
-  letters: string;
+  word: string;
+  result?: LetterState[];
+  className?: string;
 }
+export default function WordRow({
+  word = "",
+  result = [],
+  className = "",
+}: WordRowProps) {
+  const lettersRemaining = WORD_LENGTH - word.length;
+  const letters = word.split("").concat(Array(lettersRemaining).fill(""));
 
-export default function WordRow({ letters: lettersProp = "" }: WordRowProps) {
-  const lettersRemaining = LETTER_LENGTH - lettersProp.length;
-  const letters = lettersProp
-    .split("")
-    .concat(Array(lettersRemaining).fill(""));
   return (
-    <div className="grid grid-cols-5 gap-4">
-      {letters.map((char) => (
-        <CharacterBox key={char} value={char} />
+    <div className={`grid grid-cols-5 gap-4 ${className}`}>
+      {letters.map((char, index) => (
+        <CharacterBox key={index} value={char} state={result[index]} />
       ))}
     </div>
   );
 }
 
 interface CharacterBoxProps {
-  value: string;
+  value?: string;
+  state?: LetterState;
 }
+function CharacterBox({ value, state }: CharacterBoxProps) {
+  const stateStyles =
+    state == null
+      ? "border-gray-500 text-black"
+      : `${characterStateStyles[state]} text-white`;
 
-function CharacterBox({ value }: CharacterBoxProps) {
   return (
-    <div className="inline-block border-2 border-gray-500 p-4 uppercase font-bold text-2xl text-center">
+    <span
+      className={`border-2 p-2 uppercase text-center font-extrabold text-4xl before:inline-block before:content-['_'] ${stateStyles} `}
+    >
       {value}
-    </div>
+    </span>
   );
 }
+
+const characterStateStyles = {
+  [LetterState.Miss]: "border-gray-500 bg-gray-500",
+  [LetterState.Present]: "border-yellow-500 bg-yellow-500",
+  [LetterState.Match]: "border-green-500 bg-green-500",
+};
